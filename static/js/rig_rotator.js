7da1f0775f5f33e8d5129bb8222c850fbee331a6
Evan Pratten <evan@ewpratten.com>
2024-02-13 11:00:13 -0500

    Preload rig frames

--- static/js/rig_rotator.js
+++ static/js/rig_rotator.js
@@ -17,3 +17,24 @@ function handleRigNewFrame(element){
     // Update the image path
     rig_frame.src = img;
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Find all the rig rotator elements
+    var rig_rotators = document.querySelectorAll(".rig-rotator");
+    console.log(`Found ${rig_rotators.length} rig rotators`);
+
+    // Loop through each rig rotator and preload the frames
+    rig_rotators.forEach(function(rig_rotator) {
+        var rig_name = rig_rotator.getAttribute("data-name");
+        var frame_selector = rig_rotator.querySelector("input");
+        var max_frame_id = parseInt(frame_selector.max);
+        console.log(`Preloading frames for: ${rig_name}`);
+        console.log(`Max frame id: ${max_frame_id}`);
+
+        for (var i = 1; i <= max_frame_id; i++) {
+            var img = new Image();
+            img.src = `/static/images/rigs/${rig_name}/${rig_name}-${zeroPad(i, 2)}.png`;
+            console.log(`Preloading: ${img.src}`);
+        }
+    });
+});
