1001b4083582f2c43fe45984c5613966f89bd519
Evan Pratten <evan@ewpratten.com>
2024-07-10 10:06:19 -0400

    Only track share files in GC when they exist
    Analytics was tracking all requests, not just successful ones. This
    should fix that.

--- functions/share/[[file]].js
+++ functions/share/[[file]].js
@@ -10,36 +10,41 @@ export async function onRequest(context) {
     let url = new URL(context.request.url);
     let file_path = url.pathname;
 
-    // Make a call to the analytics API to track this request
-    await fetch(
-        "https://analytics.demilurii.art/api/v0/count",
-        {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": "Bearer " + context.env.GOATCOUNTER_API_KEY
-            },
-            body: JSON.stringify({
-                hits: [
-                    {
-                        location: context.request.cf.country,
-                        path: file_path,
-                        title: file_path.split("/").pop(),
-                        query: url.search,
-                        ref: context.request.headers.get("Referer"),
-                        user_agent: context.request.headers.get("User-Agent"),
-                    }
-                ],
-                no_sessions: true
-            })
-        }
-    );
-
     // Re-write URL to point to the real file server
     file_path = file_path.replace(/^\/share/, "");
     url.hostname = "share.ewu-home.com";
     url.pathname = `/miwu${file_path}`;
 
+    // Fetch the underlying file
+    let response = await fetch(url.toString(), context.request);
+
+    // Make a call to the analytics API to track this request
+    if (response.ok) {
+        await fetch(
+            "https://analytics.demilurii.art/api/v0/count",
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": "Bearer " + context.env.GOATCOUNTER_API_KEY
+                },
+                body: JSON.stringify({
+                    hits: [
+                        {
+                            location: context.request.cf.country,
+                            path: file_path,
+                            title: file_path.split("/").pop(),
+                            query: url.search,
+                            ref: context.request.headers.get("Referer"),
+                            user_agent: context.request.headers.get("User-Agent"),
+                        }
+                    ],
+                    no_sessions: true
+                })
+            }
+        );
+    }
+
     // Request the file from the file server
-    return fetch(url.toString(), context.request);
+    return response;
 }
